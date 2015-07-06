1a0cd36fd6c71dccd0323d544ae6a491051cf221
gaurav189 <lucky18389@gmail.com>
2015-07-06 16:49:15 +0530

    final code v1

---
+++ Imtcspeechservice/speechinterface.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Imtcspeechservice
+{
+    public interface speechinterface
+    {
+        void Add_Grammer(ref int result);
+
+        Microsoft.Maps.MapControl.WPF.Location getlocation(string input, ref string results);
+
+        void CalculateRouteLocation(string from, string to);
+        
+        void speechlibrray_windowclosing();
+
+        string getdirection(string start, string end, ref string distance);
+
+       
+    }
+}
